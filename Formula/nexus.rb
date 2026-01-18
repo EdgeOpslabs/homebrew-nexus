2392893ca1819c735fc1e52ee57e98faa2b95e82
iemafzalhassan <iemafzalhassan@gmail.com>
2026-01-18 17:48:04 +0530

    Update Nexus to v1.0.5

--- Formula/nexus.rb
+++ Formula/nexus.rb
@@ -1,8 +1,8 @@
 class Nexus < Formula
   desc "Nexus - the unified bridge for infrastructure context"
   homepage "https://github.com/EdgeOpslabs/nexus"
-  url "https://github.com/EdgeOpslabs/nexus/releases/download/v0.0.1/nexus_darwin_amd64.tar.gz"
-  sha256 "REPLACE_WITH_SHA256"
+  url "https://github.com/EdgeOpslabs/nexus/releases/download/v1.0.5/nexus_darwin_amd64.tar.gz"
+  sha256 "f7c2a738e91b4cdd5a41dad7c48d89507e91da4678dcd66396b0bc03636f5c4c"
   version "0.0.1"
 
   def install
