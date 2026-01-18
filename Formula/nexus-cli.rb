d93c7d5e701e863939931e702487567a8dd3494b
EdgeOpslabs CI <ci@edgeopslabs.com>
2026-01-18 13:39:48 +0000

    Update nexus-cli to v1.0.3

--- Formula/nexus-cli.rb
+++ Formula/nexus-cli.rb
@@ -2,13 +2,13 @@ class NexusCli < Formula
   desc "Nexus CLI - the unified bridge for infrastructure context"
   homepage "https://github.com/EdgeOpslabs/nexus"
   if Hardware::CPU.arm?
-    url "https://github.com/EdgeOpslabs/nexus/releases/download/v1.0.5/nexus_darwin_arm64.tar.gz"
-    sha256 "cf245e8b2a70e6b80b25963b51d8847bbab0f2d8eceeeb76675d3aac61555ec8"
+    url "https://github.com/EdgeOpslabs/nexus/releases/download/v1.0.3/nexus_darwin_arm64.tar.gz"
+    sha256 "e95d84ba7b65e43e1ced883e6ee42d68291873f48ea3c24ee36abada771ac484"
   else
-    url "https://github.com/EdgeOpslabs/nexus/releases/download/v1.0.5/nexus_darwin_amd64.tar.gz"
-    sha256 "f7c2a738e91b4cdd5a41dad7c48d89507e91da4678dcd66396b0bc03636f5c4c"
+    url "https://github.com/EdgeOpslabs/nexus/releases/download/v1.0.3/nexus_darwin_amd64.tar.gz"
+    sha256 "6330234fbe14226698b5b616c3973973938afc758580b8633751201cc54c8225"
   end
-  version "1.0.5"
+  version "1.0.3"
 
   def install
     bin.install "nexus"
