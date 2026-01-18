4e1dddd51833fae0a59b8f724280664e94739df1
iemafzalhassan <iemafzalhassan@gmail.com>
2026-01-18 22:15:25 +0530

    Fix v1.0.3 macOS shas

--- Formula/nexus-cli.rb
+++ Formula/nexus-cli.rb
@@ -3,10 +3,10 @@ class NexusCli < Formula
   homepage "https://github.com/EdgeOpslabs/nexus"
   if Hardware::CPU.arm?
     url "https://github.com/EdgeOpslabs/nexus/releases/download/v1.0.3/nexus_darwin_arm64.tar.gz"
-    sha256 "e95d84ba7b65e43e1ced883e6ee42d68291873f48ea3c24ee36abada771ac484"
+    sha256 "6330234fbe14226698b5b616c3973973938afc758580b8633751201cc54c8225"
   else
     url "https://github.com/EdgeOpslabs/nexus/releases/download/v1.0.3/nexus_darwin_amd64.tar.gz"
-    sha256 "6330234fbe14226698b5b616c3973973938afc758580b8633751201cc54c8225"
+    sha256 "e95d84ba7b65e43e1ced883e6ee42d68291873f48ea3c24ee36abada771ac484"
   end
   version "1.0.3"
 
